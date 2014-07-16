5d9e5dd64cb82c729b3df9d4639c794c76d6019f
zgleicher <zachgleicher@gmail.com>
2014-07-16 00:33:48 -0400

    bug fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 MeanStack-template
 ==================
+
--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 		next();
 	});
 
-	router.route('events');
+	router.route('/events');
 
 	app.use('/api', router);
 };
